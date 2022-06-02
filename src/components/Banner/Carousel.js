84b4594c149d28f9b6ebb035c0de532ec5e1976d
seamirea <amira.akybay@gmail.com>
2022-06-03 03:30:23 +0600

    fixed error with quote mark

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -3,91 +3,91 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import { Link } from 'react-router-dom';
-import {CryptoState} from "../../CryptoContext";
+import { CryptoState } from '../../CryptoContext';
 const useStyles = makeStyles((theme) => ({
-    carousel:{
-        height: "50%",
-        display: "flex",
-        alignItems: "center",
-    },
-    carouselItem: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        cursor: "pointer",
-        textTransform: "uppercase",
-        color: "white",
-      },
+  carousel: {
+    height: '50%',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  carouselItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    cursor: 'pointer',
+    textTransform: 'uppercase',
+    color: 'white',
+  },
 }));
 
 export function commas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 const Carousel = () => {
-    const [trending, setTrending] = useState([])
-    const classes = useStyles(); 
-    const {symbol} = CryptoState();
+  const [trending, setTrending] = useState([]);
+  const classes = useStyles();
+  const { symbol } = CryptoState();
 
-    const fetchTrendingCoins =async () => {
-        const {data} = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h');
-        setTrending(data);
-    };
-
-    console.log(trending);
-
-
-useEffect(() => {
-    fetchTrendingCoins();
-});
-    const items = trending.map((coin) => {
-        let profit = coin.price_change_percentage_24h >=0;
-        return(
-            <Link className={classes.carouselItem}
-            to={' /coins/${coin.id}'}>
-            <img
-            src={coin?.image}
-            alt={coin.name}
-            height="80"
-            style={{marginBottom: 10 }}/>
-            <span>
-                {coin?.symbol}
-                &nbsp;
-                <span>
-                    {profit && "+"} {coin?.price_change_percentage_24h?.toFixed(2)}
-                </span>
-            </span>
-            <span style={{ fontSize:22, fontWeight: 500}}>
-                {symbol}{commas(coin?.current_price.toFixed(2))};
-
-            </span>
-            </Link>
-
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h'
     );
-    })
+    setTrending(data);
+  };
 
-    const responsive = {
-        0:{
-            items: 2,
-        },
-        512: {
-            items: 4,
-        },
-    };
-  return <div className={classes.carousel}>
+  console.log(trending);
+
+  useEffect(() => {
+    fetchTrendingCoins();
+  });
+  const items = trending.map((coin) => {
+    let profit = coin.price_change_percentage_24h >= 0;
+    return (
+      <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span>
+            {profit && '+'} {coin?.price_change_percentage_24h?.toFixed(2)}
+          </span>
+        </span>
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol}
+          {commas(coin?.current_price.toFixed(2))};
+        </span>
+      </Link>
+    );
+  });
+
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+  };
+  return (
+    <div className={classes.carousel}>
       <AliceCarousel
-      mouseTracking
-      infinite
-      autoPlayInterval={1000}
-      animationDuration={1500}
-      disableDotsControls
-      responsive={responsive}
-      autoPlay
-      items={items}
-      
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        responsive={responsive}
+        autoPlay
+        items={items}
       />
-  </div>;
-  
+    </div>
+  );
 };
 
 export default Carousel;
